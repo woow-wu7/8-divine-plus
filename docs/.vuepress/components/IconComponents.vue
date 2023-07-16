a50b207fa3d7df8dac0cc60b38526d04dc19a230
woow-wu7 <woow.wu7@gmail.com>
2023-07-16 17:44:52 +0800

    docs: components/dv-icon.

--- docs/.vuepress/components/IconComponents.vue
+++ docs/.vuepress/components/IconComponents.vue
@@ -16,27 +16,28 @@ import IconJson from "../../../packages/theme-chalk/fonts/iconfont.json";
   margin: 0;
   padding: 0;
 }
+
 ul {
-  list-style: none;
   display: flex;
   flex-flow: wrap;
+  list-style: none;
 
+  box-sizing: border-box;
   border: 1px solid #eaecef;
   border-top: none;
   border-bottom: none;
-  box-sizing: border-box;
 }
 
 li {
   width: 25%;
+  padding: 10px 0;
+  box-sizing: border-box;
   border-right: 1px solid #eaecef;
   border-bottom: 1px solid #eaecef;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px 0;
-  box-sizing: border-box;
   transition: all 0.3s;
 }
 
@@ -49,9 +50,7 @@ li:hover {
   }
 }
 
-li:nth-child(4n) {
-  border-right: none;
-}
+li:nth-child(4n),
 li:nth-child(4 + n) {
   border-right: none;
 }
@@ -60,11 +59,10 @@ li:nth-child(4 + n) {
   font-size: 40px;
 }
 .dv-icon-class-name {
-  color: #99a9bf;
-  font-size: 14px;
-
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+  color: #99a9bf;
+  font-size: 14px;
 }
 </style>
